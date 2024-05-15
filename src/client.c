524f7ba794d25abec5de0ca67e489203b7285941
Laura de Koning <lade-kon@student.codam.nl>
2024-05-15 20:46:48 +0200

    added server pid in client

--- src/client.c
+++ src/client.c
@@ -6,14 +6,19 @@
 /*   By: lade-kon <lade-kon@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2024/05/15 19:29:36 by lade-kon      #+#    #+#                 */
-/*   Updated: 2024/05/15 20:36:19 by lade-kon      ########   odam.nl         */
+/*   Updated: 2024/05/15 20:46:04 by lade-kon      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk.h"
 
-int	main(void)
+int	main(int argc, char **argv)
 {
-	printf("CLIENT\n");
+	int	server_pid;
+
+	if (argc != 2)
+		return (0);
+	server_pid = ft_atoi(argv[1]);
+	printf("CLIENT: server_pid: %i\n", server_pid);
 	return (0);
 }
