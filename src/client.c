1dcc3d0d7abc3c9082871b8342f2ae7acc7c9ac8
Laura de Koning <lade-kon@student.codam.nl>
2024-05-17 11:29:54 +0200

    Added number check to client

--- src/client.c
+++ src/client.c
@@ -6,7 +6,7 @@
 /*   By: lade-kon <lade-kon@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2024/05/15 19:29:36 by lade-kon      #+#    #+#                 */
-/*   Updated: 2024/05/15 20:46:04 by lade-kon      ########   odam.nl         */
+/*   Updated: 2024/05/17 11:28:11 by lade-kon      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,11 +14,13 @@
 
 int	main(int argc, char **argv)
 {
-	int	server_pid;
+	int		server_pid;
+	char	*str;
 
-	if (argc != 2)
+	if (argc != 3 || !ft_isnumber(argv[1]))
 		return (0);
 	server_pid = ft_atoi(argv[1]);
+	str = argv[2];
 	printf("CLIENT: server_pid: %i\n", server_pid);
 	return (0);
 }
